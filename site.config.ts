119254b0ceb7414ae35a028e87a02e4e62195823
wing <huiying@resynctech.com>
2023-07-02 20:36:17 +0800

    ready for launch
    wip

--- site.config.ts
+++ site.config.ts
@@ -1,25 +1,26 @@
 import { siteConfig } from './lib/site-config'
 
+// https://chiseled-schooner-2ae.notion.site/The-Geek-Wing-82ea4337ac1d470e8684c65cf0ad073c?pvs=4
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '82ea4337ac1d470e8684c65cf0ad073c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'The Geek Wing',
+  domain: 'thegeekwing.com',
+  author: 'Wing',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // description: 'The Geek Wing',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'wing-puah',
+  linkedin: 'puahhuiying',
+  // twitter: 'transitive_bs',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -38,6 +39,7 @@ export default siteConfig({
   // environment variables. see the readme for more info
   isRedisEnabled: false,
 
+  isSearchEnabled: false,
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
   // example:
